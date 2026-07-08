00be098e42f144b71fddecf16861a3710e5883cc
DANNY <mochidanny618@gmail.com>
2026-07-08 11:26:12 +0800

    修改 vite的 config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-practice/',   // 👈 新增這一行
   plugins: [
     vue(),
     vueDevTools(),
